1cedf68e72171185c17c5f38027c4d2ab8464537
PC\RomaGZ <romagzm@gmail.com>
2026-01-31 00:05:39 +0200

    file picker webgl

--- Assets/Scripts/FilePicker.cs
+++ Assets/Scripts/FilePicker.cs
@@ -1,9 +1,10 @@
+using SFB;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using NativeFilePickerNamespace;
 using System.IO;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class FilePicker : MonoBehaviour
 {
@@ -14,12 +15,23 @@ public class FilePicker : MonoBehaviour
     public PanelHellp panelHellp;
     void Start()
     {
+#if !PLATFORM_WEBGL
         fileType = NativeFilePicker.ConvertExtensionToFileType("xlsx");
         Debug.Log("pdf's MIME/UTI is: " + fileType);
+#endif
     }
 
-   public void OnBtnOpenFileClick()
+    public void OnBtnOpenFileClick()
     {
+#if PLATFORM_WEBGL
+
+        // Open file with filter
+        var extensions = new[] {
+        new ExtensionFilter("xlsx Files", "xlsx")};
+
+        selPath = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, true)[0]; 
+
+#else
         NativeFilePicker.PickFile((path) =>
         {
             if (path == null)
@@ -32,9 +44,12 @@ public class FilePicker : MonoBehaviour
             }
                
         }, new string[] { fileType });
+#endif
     }
+
     public void OnBtnUpdateClick()
     {
+        
         if (File.Exists(selPath))
         {
             excelReaderExample.LoadAndDisplayTopPlayers(selPath);
